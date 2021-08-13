565b1d228593b458a23da0468d8c6244009b1452
Jacob White Gabauer <jakewithjack@gmail.com>
2021-08-12 21:25:04 -0700

    Created site - First commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const typed = new Typed('#typed', {
+    strings: ['Cheapest','Most Reliable', 'Most Reputable', 'Best'],
+    smartBackspace: true, // Default value
+    typeSpeed: 60,
+    loop: true,
+    backDelay: 1100,
+    backSpeed: 30,
+  });
+
+// const aboutPage = document.querySelector('#about');
+// aboutPage.addEventListener('DOMContentLoaded', function(){
+//     console.log("Loaded")
+// })
+
+// document.addEventListener('DOMContentLoaded', function(){
+//     console.log("LoadedDoc");
+// })
